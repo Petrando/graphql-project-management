77013cc6293ba1ebc641261346ef35c9a4d5c682
Petrando <petrandorichard@gmail.com>
2024-07-31 19:37:48 +0800

    fix: export Home page components

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -2,3 +2,5 @@ import Projects from "./Projects";
 import Clients from "./Clients";
 import AddClientModal from "./AddClientModal";
 import AddProjectModal from "./AddProjectModal";
+
+export { Projects, Clients, AddProjectModal, AddClientModal }
